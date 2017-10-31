c79ac8090136c6e9b67a074250718ae807934728
floflo199721 <florian.martines@ynov.com>
2017-10-31 16:57:43 +0100

    JE TE VOIE

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,32 +1,57 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
+  providers: [Camera],
   templateUrl: 'home.html'
 })
 
 export class HomePage {
 
 
+  public base64Image: string;
+
+
   app: AppData;
 
-  constructor(public navCtrl: NavController) {
-    this.app = new AppData("Nom", 0.3);
+  constructor(private camera:Camera, public navCtrl: NavController) {
+    this.app = new AppData("Nom", 0.4);
 
   }
 
-    presentActionSheet() {
-console.log("bouille");
+  presentActionSheet() {
+    console.log("bouille");
 
   }
+
+  options: CameraOptions = {
+    quality: 100,
+    correctOrientation: true,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  takePicture(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+
+
 }
+
 
 class AppData{
 
-nom:String;
-version:Number;
+  nom:String;
+  version:Number;
 
-constructor(public name:String, public versionnig:Number) {}
+  constructor(public name:String, public versionnig:Number) {}
 
 }
